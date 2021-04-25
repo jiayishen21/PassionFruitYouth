663f3b46068a417c7a9191d0a29a7e2282214ca3
Lucas Shen <lucas.shxn@gmail.com>
2021-04-24 22:16:01 -0400

    Navbar for version 2.0

---
+++ PassionFruitYouth_V2.0/passion-fruit-youth-v2/src/app/navbar/navbar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  sections: Section[];
+
+  constructor() { 
+    this.sections = [
+      new Section('Home', ''),
+      new Section('Oppurtunities', '/oppurtunities'),
+      new Section('Events', '/events'),
+      new Section('Meet the Team', '/meet-the-team'),
+      new Section('About', '/about')
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
+class Section {
+  private _name: String;
+  private _href: String;
+
+  constructor(name:String, href:String) {
+    this._name = name;
+    this._href = href;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get href() {
+    return this._href;
+  }
+}
