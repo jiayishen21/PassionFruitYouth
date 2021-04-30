a6f400bb27b91d31719b22864caa2dfe67c11453
Lucas Shen <lucas.shxn@gmail.com>
2021-04-29 22:57:54 -0400

    cleanup

--- PassionFruitYouth_V2.0/passion-fruit-youth-v2/src/app/app.module.ts
+++ PassionFruitYouth_V2.0/passion-fruit-youth-v2/src/app/app.module.ts
@@ -5,13 +5,6 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
-import { HomePageComponent } from '../components/home-page/home-page.component';
-import { LearnMoreComponent } from '../components/learn-more/learn-more.component';
-import { OpportunitiesButtonComponent } from '../components/opportunities-button/opportunities-button.component';
-import { EventsButtonComponent } from '../components/events-button/events-button.component';
-import { ClassroomTransitionComponent } from '../components/classroom-transition/classroom-transition.component';
-import { MotivationPageComponent } from '../components/motivation-page/motivation-page.component';
-import { DescriptionPageComponent } from '../components/description-page/description-page.component';
 import { HomeComponent } from '../components/home/home.component';
 import { OpportunitiesComponent } from '../components/opportunities/opportunities.component';
 import { SelectionPageComponent } from '../components/selection-page/selection-page.component';
@@ -23,13 +16,6 @@ import { BusinessPageComponent } from '../components/business-page/business-page
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomePageComponent,
-    LearnMoreComponent,
-    OpportunitiesButtonComponent,
-    EventsButtonComponent,
-    ClassroomTransitionComponent,
-    MotivationPageComponent,
-    DescriptionPageComponent,
     HomeComponent,
     OpportunitiesComponent,
     SelectionPageComponent,
